0476ba12f1247085a7df5f8fd571d668999f47be
Vinicius <81567529+viniciusozawa@users.noreply.github.com>
2025-03-25 16:49:03 -0300

    Update comandos_modelo_fisico.sql

--- comandos_modelo_fisico.sql
+++ comandos_modelo_fisico.sql
@@ -3,7 +3,9 @@ CREATE DATABASE exercicio2E;
 SHOW DATABASES;
 
 USE exercicio2e;
-
+#Criar a tabela Cliente com 3 atributos(campos)
+#Definar quais campos serao obrigatorios(not null)
+#indicar qual será a chave primaria, usando primary key	
 CREATE TABLE cliente(
 	
 	codCliente INT NOT NULL AUTO_INCREMENT,
@@ -13,3 +15,4 @@ CREATE TABLE cliente(
 	
 );
 DESC cliente;
+#usando o workbrench é possivel obter o MER pelo menu database reverse engineer, na mesma porta usanda no heidi
